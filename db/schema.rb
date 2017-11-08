ef9034e0b5f8baf8f16bd02fda8427e30ad311b6
kennethteh90 <kennethteh90@gmail.com>
2017-11-08 15:41:02 +0800

    added Product model, products controller and views

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171107090644) do
+ActiveRecord::Schema.define(version: 20171108063229) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "products", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "brand", null: false
+    t.text "description", null: false
+    t.decimal "price", precision: 8, scale: 2, null: false
+    t.integer "quantity", null: false
+    t.string "size"
+    t.string "color"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
