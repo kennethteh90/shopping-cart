be68981eb7a4e218568d2847824d788f50e404ed
jsio001 <jsio001@e.ntu.edu.sg>
2017-11-13 23:43:02 +0800

    f.association:photos

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -11,6 +11,7 @@ class Admin::ProductsController < AdminController
 
   def create
     @product = Product.new(product_params)
+    @product.photos.build
     @cat_array = params.dig(:product, :category_ids).reject(&:empty?)
     @cat_array.each do |cat|
       @category = Category.find(cat)
@@ -55,7 +56,7 @@ class Admin::ProductsController < AdminController
   private
 
     def product_params
-      params.require(:product).permit(:name, :brand, :description, :price, :quantity, :size, :color, :category_ids)
+      params.require(:product).permit(:name, :brand, :description, :price, :quantity, :size, :color, :category_ids, :photo [:filelocation])
     end
 
 end
