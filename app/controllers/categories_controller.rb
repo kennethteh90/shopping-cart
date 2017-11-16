4d3509bd9ac1393294ce09cb01e34a263c5950c6
jsio001 <jsio001@e.ntu.edu.sg>
2017-11-16 13:52:32 +0800

    fix non-admin view

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,7 +2,17 @@ class CategoriesController < ApplicationController
 
   def show
     @category = Category.find(params[:id])
-    @products = @category.products.all
+    @products = if params[:search]
+        @category.products.where("name ILIKE :query OR brand ILIKE :query OR description ILIKE :query", query: "%#{params[:search][:term]}%" )
+      else
+        @category.products.all
+      end
   end
+
+  private
+
+    def search_params
+      params.require(:search).permit(:term)
+    end
 
 end
